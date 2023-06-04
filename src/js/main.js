714a4127abea6a430f0afb0a8ed5128fe4d9f106
NyTakoeSebe <evd.matvey@mail.ru>
2023-06-04 15:35:49 +0700

    why to stop mobile layout

--- src/js/main.js
+++ src/js/main.js
@@ -19,3 +19,22 @@ function hideMobileNavigationHandler() {
     document.body.classList.remove('navigation-open');
   }, 100);
 }
+
+// why to stop slider
+const swiper = new Swiper('.stop__slider', {
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  pagination: {
+    el: '.stop__slider-pagination.swiper-pagination',
+    clickable: true,
+    renderBullet: function () {
+      return `<span class="stop__slider-bullet swiper-pagination-bullet"></span>`;
+    },
+  },
+  navigation: {
+    nextEl: '.stop__slider-next',
+    prevEl: '.stop__slider-prev',
+  },
+});
